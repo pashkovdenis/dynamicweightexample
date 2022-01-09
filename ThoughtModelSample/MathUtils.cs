e376c4394118505119a744751dedc070560f27b3
denis <pashkovdenis@gmail.com>
2022-01-10 00:28:35 +0200

    init

---
+++ ThoughtModelSample/MathUtils.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThoughtModelSample
+{
+    internal static  class MathUtils
+    {
+        public static double CalculateWebersForce(double multiplier, double force, double low) => multiplier * Math.Log10(force / low);
+        public static double Derivative(double x) => x * (1 - x);
+        public static double Sigmoid(double value) => 1.0f / (1.0f + (float)Math.Exp(-value));
+
+        /// <summary>
+        /// Softmax vector normalization aka probability distribution
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static double[] SoftMax(double[] input)
+        {
+            var z_exp = input.Select(Math.Exp);
+            var sum_z_exp = z_exp.Sum();
+            return z_exp.Select(i => i / sum_z_exp).ToArray();
+        }
+
+        /// <summary>
+        /// Calculate force dependent on distance value
+        /// </summary>
+        /// <param name="distance"></param>
+        /// <returns></returns>
+        public static double DistanceRegression(double distance)
+        {
+            return CalculateWebersForce(0.01d, distance, .222d);
+        }
+
+
+        /// <summary>
+        /// Calculate random normal distribution
+        /// </summary>
+        /// <param name="numberOfInputs"></param>
+        /// <returns></returns>
+        public static double CalculateRandomDistribution(int numberOfInputs)
+        {
+            var std = Math.Sqrt(2.0 / numberOfInputs);
+            var dist = new List<double>();
+
+            for (int i = 0; i < numberOfInputs * 2; i++)
+            {
+                var rand = new Random();
+                double normRand = Phi(rand.NextDouble());
+                dist.Add(normRand * std);
+            }
+
+            return dist.Average();
+        }
+
+
+        static double Phi(double x)
+        {
+            // constants
+            double a1 = 0.254829592;
+            double a2 = -0.284496736;
+            double a3 = 1.421413741;
+            double a4 = -1.453152027;
+            double a5 = 1.061405429;
+            double p = 0.3275911;
+
+            // Save the sign of x
+            int sign = 1;
+            if (x < 0)
+                sign = -1;
+            x = Math.Abs(x) / Math.Sqrt(2.0);
+
+            // A&S formula 7.1.26
+            double t = 1.0 / (1.0 + p * x);
+            double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
+
+            return 0.5 * (1.0 + sign * y);
+        }
+
+
+
+    }
+}
---
+++ ThoughtModelSample/Program.cs
@@ -0,0 +1,206 @@
+﻿using ThoughtModelSample;
+Console.WriteLine("Thought container example");
+
+
+
+
+
+var thought = new Thought
+{
+    Decisions = new List<Decision>
+        {
+            new Decision
+            {
+                Answer = "True",
+                Id =1,
+                Symbols = new List<Symbol>{new Symbol { Word = "1"}, new Symbol { Word = "1" } }
+            },
+            new Decision
+            {
+                Answer = "False",
+                Id = 2,
+                Symbols = new List<Symbol>{new Symbol { Word = "1"}, new Symbol { Word = "0" } }
+            },
+            new Decision
+            {
+                Answer = "True",
+                Id = 3,
+                Symbols = new List<Symbol>{new Symbol { Word = "0"}, new Symbol { Word = "0" } }
+            },
+            new Decision
+            {
+                Answer = "False",
+                Id = 4,
+                Symbols = new List<Symbol>{new Symbol { Word = "0"}, new Symbol { Word = "1" } }
+            },
+            new Decision
+            {
+                Answer = "Hello",
+                Id = 5,
+                Symbols = new List<Symbol>{new Symbol { Word = "Hi"}, new Symbol { Word = "There" } }
+            },
+        }
+};
+
+thought.Decisions.ForEach(x => x.ResetWeights());
+
+
+thought.Reinforce(new[] { "0", "0" }, 3);
+thought.Reinforce(new[] { "1", "0" }, 2);
+thought.Reinforce(new[] { "0", "1" }, 4);
+thought.Reinforce(new[] { "1", "1" }, 1);
+thought.Reinforce(new[] { "Hi", "There" }, 5);
+
+Validate(thought.GetAnswers(new[] { "1", "1" }).First().Answer, "True");
+Validate(thought.GetAnswers(new[] { "1", "0" }).First().Answer, "False");
+Validate(thought.GetAnswers(new[] { "0", "1" }).First().Answer, "False");
+Validate(thought.GetAnswers(new[] { "0", "0" }).First().Answer, "True");
+Validate(thought.GetAnswers(new[] { "Hi", "There" }).First().Answer, "Hello");
+
+
+
+
+static void Validate(string result, string expected)
+{
+    Console.WriteLine(result == expected ? "Valid" : "False");
+
+
+    if (result != expected)
+    {
+        Console.ReadKey();
+    }
+
+}
+
+// Ready for action over here 
+Console.WriteLine("Ready for action");
+
+
+
+// Region of the custom structures.
+public class Symbol
+{
+    public string Word { get; init; }
+
+    public double Weight { get; set; }
+
+}
+
+/// <summary>
+/// Represent a single decision within one thought
+/// this decision can act as one of possible choices depending on stimulus
+/// </summary>
+///
+
+public class Decision
+{
+    public long Id { get; init; }
+    public List<Symbol> Symbols { get; init; } = new();
+
+    public double Bias { get; set; }
+    public double Delta { get; set; }
+    public string Answer { get; set; }
+
+    // ResetWeights
+    public void ResetWeights()
+    {
+        foreach (var symbol in Symbols)
+        {
+            symbol.Weight = MathUtils.CalculateRandomDistribution(10);
+        }
+
+        Bias = MathUtils.CalculateRandomDistribution(Symbols.Count);
+    }
+
+    public void UpdateWeights()
+    {
+        Bias += Delta;
+
+        foreach (var symbol in Symbols)
+        {
+            symbol.Weight += Delta;
+        }
+
+    }
+
+
+    // TODO: Pass normalization action here ?
+    public double Stimulate(string[] words)
+    {
+        var matchedSymbols = Symbols.Where(s => words.Contains(s.Word));
+
+        var score = matchedSymbols.Sum(x => x.Weight) + Bias;
+
+        return MathUtils.Sigmoid(score);
+    }
+
+
+    public override string ToString() => Answer;
+}
+
+
+/// <summary>
+/// Logical grouping of decisions
+/// cluster of decisions that represents a thought
+/// </summary>
+public class Thought
+{
+    public List<Decision> Decisions { get; init; } = new();
+
+    public Output[] GetAnswers(string[] stimulus) => Decisions
+        .Select(x => new Output(x.Stimulate(stimulus), x.Answer, x.Id))
+        .OrderByDescending(x => x.Score)
+        .ToArray();
+
+    /// <summary>
+    /// Reinforce single thought
+    /// Updates symbols weights in order to get predicted output for a single thought
+    /// basically it is a kind of back-propagation.
+    /// </summary>
+    /// <param name="stimulus"></param>
+    /// <param name="expected"></param>
+    public void Reinforce(string[] stimulus, long expectedDecisionId)
+    {
+        var repeatTimes = Decisions.Count * 1300;
+
+        for (var i = 0; i < repeatTimes; i++)
+        {
+            var currentAnswers = GetAnswers(stimulus);
+            var answer = currentAnswers.First();
+
+            var targetDecision = Decisions.First(d => d.Id == expectedDecisionId);
+            var targetAnswer = currentAnswers.First(a => a.DecisionId == targetDecision.Id);
+
+            targetDecision.Delta = MathUtils.Derivative(targetAnswer.Score) * (targetAnswer.Score * answer.Score);
+            targetDecision.UpdateWeights();
+
+            var otherDecisions = currentAnswers.Where(x => x.DecisionId != expectedDecisionId)
+                .Select(x => x.DecisionId)
+                .ToArray();
+
+            var updateDelta = MathUtils.Derivative(answer.Score) * targetDecision.Delta * targetAnswer.Score;
+
+            foreach (var dec in otherDecisions)
+            {
+                var currentDecision = Decisions.First(d => d.Id == dec);
+
+                currentDecision.Delta = updateDelta;
+                currentDecision.UpdateWeights();
+            }
+        }
+    }
+}
+
+/// <summary>
+/// Represent a thought's output structure 
+/// </summary>
+/// <param name="Score"></param>
+/// <param name="Answer"></param>
+public record struct Output(double Score, string Answer, long DecisionId)
+{
+    public override string ToString() => $"Id: {DecisionId} ; {Answer}({Score})";
+}
+
+
+
+
